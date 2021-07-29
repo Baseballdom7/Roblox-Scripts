2307243ce905587ad39c12fbc2ab97ee423d85c4
Dominic <85628816+Baseballdom7@users.noreply.github.com>
2021-07-29 09:02:36 -0500

    Create HealthBar.lua

---
+++ HealthBar.lua
@@ -0,0 +1,10 @@
+**This is the Health Bar script**
+**This is the LocalScript(inside of the last frame)**
+
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid =  character:WaitForChild("Humanoid")
+
+humanoid.HealthChanged:Connect(function(Damage)
+    script.Parent.Size = UDim2.new(Damage / humanoid.MaxHealth, 0,1,0)
+end)
